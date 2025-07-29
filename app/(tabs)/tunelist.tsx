2f14fe3e94c2832ae64dc09291ca6fe014ff66a2
mullerai <mullerai@tcd.ie>
2025-07-29 14:30:37 +0100

    Added tunelist page that displays tunes

--- app/(tabs)/tunelist.tsx
+++ app/(tabs)/tunelist.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const app = () => {
+    var [tunelistJSON, setTunelistJSON] = useState<any[]>([]);
+    const retrieveTuneLists = async () => {
+        try {
+            const value = await AsyncStorage.getItem('tunelist'); // check if username is already stored
+            if (value !== null) {
+                setTunelistJSON(JSON.parse(value));
+            }
+        } catch (error) {
+            console.log(error);
+        }
+      }
+      useEffect(() => {
+        retrieveTuneLists();
+      }, []);
   return (
     <View style={styles.container}>
-
+        <ScrollView>
+        {
+            tunelistJSON.map((tune: any, key) => (
+                <Text key={key}>{tune.name}</Text>
+            ))
+        }
+        </ScrollView>
     </View>
   )
 }
